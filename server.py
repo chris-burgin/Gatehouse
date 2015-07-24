11ba3a5b8b8ed7471a4647b3d8d77155f5d9bf56
ChrisBurgin <chrisburgin95@gmail.com>
2015-07-24 17:59:06 -0400

    Optimization Updates For Server

--- server.py
+++ server.py
@@ -15,8 +15,9 @@ pinList = [4]
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+
+#DATABASE
 def connect_db():
-    """Connects to the specific database."""
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
@@ -32,94 +33,139 @@ def teardown_request(exception):
         db.close()
 
 def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
     if not hasattr(g, 'sqlite_db'):
         g.sqlite_db = connect_db()
     return g.sqlite_db
 
+
+#ROUTES
+#INDEX
 @app.route('/')
 def index():
-    if session.get('logged_in') == True:
+    if (session.get('logged_in') == True):
         return render_template('index.html')
     else:
         return render_template('login.html')
 
+#LOGIN
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
-    error = None;
-    if request.method == 'POST':
+    if (request.method == 'POST'):
+        #Database Login
         db = get_db()
-        userFound = False;
         user = [request.form['username'], request.form['password']]
-        cur = db.execute('select username, password from users order by id desc')
+        cur = db.execute('select username, password, admin from users order by id desc')
         entries = cur.fetchall()
-        for item in entries:
-            if user[0] == item[0]:
-                if user[1] == item[1]:
-                    userFound = True;
-                    break
+        for databaseItem in entries:
+            if user[0] == databaseItem[0]:
+                if user[1] == databaseItem[1]:
+                    session['logged_in'] = True
+                    return render_template('index.html')
 
-        if userFound == True:
-            session['logged_in'] = True
-            return render_template('index.html')
+        #Default Login Information
+        if (request.form['username'] == app.config['USERNAME']):
+            if (request.form['password'] == app.config['PASSWORD']):
+                session['logged_in'] = True
+                return redirect(url_for('index'))
+        return render_template('login.html', error=True)
 
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'invalid'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'invalid'
-        else:
-            session['logged_in'] = True
-            return redirect(url_for('index'))
-        return render_template('login.html', error=error)
-   
-    if request.method == 'GET' and session.get('logged_in') == True:
-        return redirect(url_for('index'))
-    else:
+    if request.method == 'GET':
         return render_template('login.html')
 
+#LOGOUT
 @app.route('/logout/')
 def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
     return redirect(url_for('index')) 
 
+#USERS
 @app.route('/user/', methods=['GET', 'POST'])
 def users():
-    if session.get('logged_in') != True:
-        return redirect(url_for('login'))
-
+    verifyStatus()
     if request.method == 'GET':
         db = get_db()
         cur = db.execute('select username, password from users order by id desc')
         entries = cur.fetchall()
         return render_template('adduser.html', entries=entries)
-
-    g.db.execute('insert into users (username, password) values (?, ?, ?)',
+    g.db.execute('insert into users (username, password, admin) values (?, ?, ?)',
                  [request.form['username'], request.form['password'], request.form['adminuser']])
     g.db.commit()
     return redirect(url_for('index'))
 
-@app.route('/toggledoor/', methods=['GET', 'POST'])
+#TOGGLEDOOR
+@app.route('/toggledoor/', methods=['POST'])
 def toggledoor():
-    if session.get('logged_in') != True:
-        return redirect(url_for('login'))
-
+    verifyStatus()
+    #Possibly Useless, Do some testing to make sure this page only accepts "POST" requests
     if request.method == 'GET':
         return redirect(url_for('index'))
-
-    #GPIO.output(4, GPIO.LOW)
-    #time.sleep(.2);
-    #doorcleanup(); 
+    openDoor()
     return redirect(url_for('index'))
 
-def doorcleanup():
+#OPENDOOR
+def openDoor():
+    GPIO.output(4, GPIO.LOW)
+    time.sleep(.2); 
     GPIO.cleanup()
     GPIO.setmode(GPIO.BCM)
     for i in pinList:
         GPIO.setup(i, GPIO.OUT)
         GPIO.output(i, GPIO.HIGH)
+
+
+#VERIFY LOGIN
+def verifyStatus():
+    if session.get('logged_in') != True:
+        return redirect(url_for('login'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
    # for i in pinList: 
