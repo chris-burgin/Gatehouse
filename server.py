8871cae6e97ba9c991dac0a16948376060f675d3
ChrisBurgin <chrisburgin95@gmail.com>
2015-08-22 23:15:40 -0400

    Server File Comments

--- server.py
+++ server.py
@@ -12,17 +12,25 @@ from modules.settings import Settings
 
 
 # GLOBAL VARIABES
+DEBUG = True  # This needs to be commented out in master for security.
+
+# Master login
 settings = Settings()
-DEBUG = True # This needs to be commented out in master for security.
 USERNAME = settings.username()
 PASSWORD = settings.password()
+
+# Secret Key for sessions
 SECRET_KEY = str(random.random())
+
+# Init App
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./tmp/database.db'
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
+# GET - Shows main page
+# POST - None
 def index():
     if not user.loggedIn():
         return redirect(url_for('login'))
@@ -31,101 +39,133 @@ def index():
 
 
 @app.route('/login/', methods=['GET', 'POST'])
+# GET - Shows login page
+# POST - Logs user in
 def login():
-
-    # Redirects on GET Request
+    # Redirects on GET request
     if request.method == 'GET':
         return render_template('login.html')
 
+    # Gets login info from page
     username = request.form['username']
     password = request.form['password']
 
+    # fetches user from database
     databaseUser = database.getUser(username)
     if databaseUser:
         # Checks username and password against the database
         if (username == databaseUser.username and
                 security.encrypt(password) == databaseUser.password):
-
+            # Checks if user is temporary and if the date is expired
             if (databaseUser.experationDate != 'False' and
                     user.isExpired(databaseUser.experationDate)):
+                # Returns expired message
                 error = "Your temporary account has expired."
                 return render_template('login.html', error=error)
 
-            # Updates Logged In Status and Admin Status
+            # Logs user in
             user.login()
+            # Checks if user is admin
             if databaseUser.admin is True:
+                # Sets admin status
                 user.setAdmin()
+            # Returns Index
             return redirect(url_for('index'))
 
-    # Checks Applicaton Admin
+    # Checks if master user
     if (request.form['username'] == app.config['USERNAME'] and
        request.form['password'] == app.config['PASSWORD']):
+            # Logs user in
             user.login()
+            # Sets admin status
             user.setAdmin()
+            # Returns Index
             return redirect(url_for('index'))
 
-    # Returns Invalid Login
+    # Returns invalid login message
     error = "Invalid Username or Password."
     return render_template('login.html', error=error, username=username)
 
 
-@app.route('/logout/')
+@app.route('/logout/', methods=['GET'])
+# GET - Logs user out
+# POST - None
 def logout():
-    # Logs The User Out
+    # Logs user out
     user.logout()
+    # Returns index
     return redirect(url_for('index'))
 
 
 @app.route('/users/', methods=['GET', 'POST'])
+# GET - Shows user creation and lists users
+# POST - Creates Users
 def users():
+    # Checks if logged in
     if not user.loggedIn():
+        # Returns logged in if not
         return redirect(url_for('login'))
 
+    # Checks if admin
     if not user.isAdmin():
+        # Returns index if not
         return redirect(url_for('index'))
 
+    # Checks if GET request
     if request.method == 'GET':
+        # Returns users and success message if available
         success = request.args.get('success')
         return render_template('users.html', users=database.userList(),
                                success=success)
 
-    # Verify Username
+    # Get username from page
     username = request.form['username']
+    # Gets user from database
     databaseUser = database.getUser(username)
+    # Checks if user exists
     if (databaseUser is not None):
+        # Returns error message
         error = "User already exists"
         return render_template('users.html', users=database.userList(),
                                error=error)
 
+    # Checks if username is valid
     if (not security.usernameStrength(username)):
         error = "Username must be at least 3 characters long"
         return render_template('users.html', users=database.userList(),
                                error=error)
 
-    # Verify Password
+    # Get password from page
     password = request.form['password']
+    # Checks if password is valid
     if security.passwordStrength(password) is True:
+        # Encrypts password
         password = security.encrypt(password)
     else:
+        # Returns invalid password
         error = "Your password must be at least 6 characters long and\
                  contain a number"
         return render_template('users.html', users=database.userList(),
                                error=error, username=username)
 
-    # Get Admin Status
+    # Checks if admin is checked
     if request.form.get('adminuser'):
+        # A checkbox does not return true or false, so we set it here
         isAdmin = True
     else:
         isAdmin = False
 
-    # Get Temp User Status
+    # Checks if temp user is checked
     if request.form.get('tempuser'):
+        # Sets experation date from page
         experationDate = str(request.form.get('dateTmp'))
     else:
         experationDate = 'False'
 
-    # Create User
+    # Creates user
     database.createUser(username, password, isAdmin, experationDate)
+
+    # Returns success message
     success = ("%s was created!")
     success = success % username
     return render_template('users.html', users=database.userList(),
@@ -133,6 +173,8 @@ def users():
 
 
 @app.route('/edituser/', methods=['POST'])
+# GET - None
+# POST - Edits User
 def edituser():
     # Checks if the user is logged in
     if not user.loggedIn():
@@ -147,14 +189,21 @@ def edituser():
 
     # Checks if username already exists
     username = request.form['username']
+
+    # Gets user from the database
     databaseUser = database.getUser(username)
+
+    # Checks if username already exists
     if (databaseUser is not None):
+        # Returns username error message
         error = "User already exists"
         return render_template('users.html', users=database.userList(),
                                error=error)
 
-    # Checks password before encrpyting
+    # Gets password from the page, already checked in javascript so no
+    # checking needs to happen here.
     if request.form['password']:
+        # Encrpys the password
         password = security.encrypt(request.form['password'])
     else:
         password = None
@@ -165,33 +214,45 @@ def edituser():
     else:
         isAdmin = False
 
-    # Checks if user is a tmp user
+    # Gets temp checkbox status from page
     tmpStatus = request.form.get('dateTmp')
+
+    # Checks if temp is checked
     if (tmpStatus):
+        # Sets experation date
         experationDate = str(tmpStatus)
     else:
-        # Uses string for Jinja Template (May be able to use actuall False)
+        # Uses string for jinja template (May be able to use actual False)
         experationDate = 'False'
 
-    # Edits User
+    # Edits user, returns true if success
     if database.editUser(userID, username, password, isAdmin, experationDate):
+
+        # Sends success message
         success = 'User Updated!'
         return redirect(url_for('users', success=success))
     else:
+        # Sends error message
         error = 'An error has occured.'
         return redirect(url_for('users', error=error))
 
 
 @app.route('/removeuser/', methods=['POST'])
+# GET - None
+# POST - Removes the user
 def removeuser():
+    # Checks if user is logged in
     if not user.loggedIn():
         return redirect(url_for('login'))
 
+    # Checks if user is admin
     if not user.isAdmin():
         return redirect(url_for('index'))
 
+    # Gets user to remove from json object
     userID = request.json['userID']
 
+    # Removes user, returns a message to page if sucessfull
     if database.removeUser(userID):
         return 'success'
     else:
@@ -199,13 +260,21 @@ def removeuser():
 
 
 @app.route('/toggledoor/', methods=['POST'])
+# GET - None
+# POST - Toggles the status of the door
 def toggledoor():
+    # Checks if user is logged in
     if not user.loggedIn():
         return redirect(url_for('login'))
+
+    # Loggles Door
     garage.toggleDoor()
+
+    # Reloads index
     return redirect(url_for('index'))
 
 
+# INIT the application
 if __name__ == "__main__":
     # Class Instances
     security = Security()
